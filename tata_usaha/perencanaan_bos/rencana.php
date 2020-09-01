8ffe7be834ce82da7f5874961ed5a5f1a62c31c9
Iqbal Ramadhani <iqbal.ramadhani55@gmail.com>
2020-09-01 07:00:55 +0700

    up

--- tata_usaha/perencanaan_bos/rencana.php
+++ tata_usaha/perencanaan_bos/rencana.php
@@ -24,13 +24,9 @@ if (empty($data['jumlah_siswa']) || empty($data['dana_siswa'])) {
 $crud->simpan('bos_rkas_rencana', $data);
 $standar = $crud->read_data('tbl_standar_nasional');
 
-$total_dana_rencana1 = $crud->query("SELECT SUM(jumlah) jumlah FROM bos_rkas JOIN bos_rkas_detail ON bos_rkas.id_bos_rkas = bos_rkas_detail.bos_rkas WHERE bos_rkas.tahun_ajaran = '2017'")[0]['jumlah'];
-$total_dana_rencana2 = $crud->query("SELECT SUM(jumlah) jumlah FROM bos_rkas JOIN bos_rkas_detail ON bos_rkas.id_bos_rkas = bos_rkas_detail.bos_rkas WHERE bos_rkas.tahun_ajaran = '2018'")[0]['jumlah'];
+
 
 foreach ($standar as $st) {
-
-
-
   $persentase = array(
     'tahun_ajaran' => $data['tahun'],
     'npsn' => $st['idsnp'],
@@ -42,8 +38,11 @@ foreach ($standar as $st) {
   if ($data['tahun'] > 2018) {
     $tahun1 = $data['tahun'] - 1;
     $tahun2 = $data['tahun'] - 2;
+    $total_dana_rencana1 = $crud->query("SELECT SUM(jumlah) jumlah FROM bos_rkas JOIN bos_rkas_detail ON bos_rkas.id_bos_rkas = bos_rkas_detail.bos_rkas WHERE bos_rkas.tahun_ajaran = '" . $tahun2 . "'")[0]['jumlah'];
+    $total_dana_rencana2 = $crud->query("SELECT SUM(jumlah) jumlah FROM bos_rkas JOIN bos_rkas_detail ON bos_rkas.id_bos_rkas = bos_rkas_detail.bos_rkas WHERE bos_rkas.tahun_ajaran = '" . $tahun1 . "'")[0]['jumlah'];
     $dana_rencana1 = $crud->query("SELECT SUM(jumlah) jumlah FROM bos_rkas JOIN bos_rkas_detail ON bos_rkas.id_bos_rkas = bos_rkas_detail.bos_rkas WHERE bos_rkas.npsn = '" . $st['idsnp'] . "' AND bos_rkas.tahun_ajaran = '" . $tahun2 . "'")[0]['jumlah'];
-    $dana_rencana2 = $crud->query("SELECT SUM(jumlah) jumlah FROM bos_realisasi_rekapitulasi JOIN bos_realisasi_detail_komponen ON bos_realisasi_rekapitulasi.id_bos_realisasi_rekapitulasi = bos_realisasi_detail_komponen.relasi_id WHERE bos_realisasi_rekapitulasi.idsnp = '" . $st['idsnp'] . "' AND bos_realisasi_rekapitulasi.tahun_ajaran = '" . $tahun1 . "'")[0]['jumlah'];
+    $dana_rencana2 = $crud->query("SELECT SUM(jumlah) jumlah FROM bos_rkas JOIN bos_rkas_detail ON bos_rkas.id_bos_rkas = bos_rkas_detail.bos_rkas WHERE bos_rkas.npsn = '" . $st['idsnp'] . "' AND bos_rkas.tahun_ajaran = '" . $tahun1 . "'")[0]['jumlah'];
+    // $dana_rencana2 = $crud->query("SELECT SUM(jumlah) jumlah FROM bos_realisasi_rekapitulasi JOIN bos_realisasi_detail_komponen ON bos_realisasi_rekapitulasi.id_bos_realisasi_rekapitulasi = bos_realisasi_detail_komponen.relasi_id WHERE bos_realisasi_rekapitulasi.idsnp = '" . $st['idsnp'] . "' AND bos_realisasi_rekapitulasi.tahun_ajaran = '" . $tahun1 . "'")[0]['jumlah'];
     $persentase1 = ($dana_rencana1 / $total_dana_rencana1) * 100;
     $persentase2 = ($dana_rencana2 / $total_dana_rencana2) * 100;
     $persentase['persentase'] = ($persentase1 + $persentase2) / 2;
